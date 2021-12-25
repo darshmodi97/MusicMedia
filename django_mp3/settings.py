9c5e938151c84d65b1e2e92ee17edaa49c6f43fb
Darsh Modi <darsh.modi1111@gmail.com>
2021-12-25 16:26:57 +0530

    minor change done

--- django_mp3/settings.py
+++ django_mp3/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'zj*hz3-!y3xi#biczvr^@%kx089bpvnmxvuiih5&ez!drd=fb@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
