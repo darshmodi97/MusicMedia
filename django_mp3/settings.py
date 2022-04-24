54736f77700d327632660004d65d9f348f8050e4
Darsh Modi <darsh.modi1111@gmail.com>
2022-04-24 17:39:42 +0530

    minor fix

--- django_mp3/settings.py
+++ django_mp3/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
 }
 
 dj_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(dj_database_url)
+DATABASES['default'].update(dj_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
